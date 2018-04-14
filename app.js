a48701f7c8d97765ce283dbde74d213e79407492
MichaelJamesParsons <mjay.parsons@gmail.com>
2018-04-14 13:21:45 -0400

    Added oauth demo

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const expressHandlebars = require('express-handlebars')
+const Ustream = require('ustream-sdk')
+const app = express()
+
+app.engine('handlebars', expressHandlebars())
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+
+/**
+ * The app's entry point.
+ *
+ * Renders a button that links to Ustream's Oauth login page. This application
+ * assumes your USTREAM_REDIRECT_URI returns to:
+ *
+ *    https://yourDomain.com/channels
+ */
+app.get('/', (req, res) => {
+  const oauthParams = {
+    response_type: "token",
+    client_id: process.env.USTREAM_CLIENT_ID,
+    redirect_uri: process.env.USTREAM_REDIRECT_URI,
+    token_type: "bearer"
+  }
+
+  // Append oauthParams as query string parameters to the authorization url.
+  const endpoint = Object.keys(oauthParams).reduce((queryString, param) => {
+    return `${queryString}&${param}=${oauthParams[param]}`
+  }, `https://www.ustream.tv/oauth2/authorize?`)
+
+  res.render('index', { endpoint })
+})
+
+/**
+ * The endpoint where your OAuth redirect should send the user after
+ * they have logged in to their Ustream account.
+ */
+app.get('/channels', (req, res) => {
+  const { access_token, token_type, expires_in } = req.query
+  const ustream = new Ustream({
+    type: 'oauth_bearer',
+    access_token,
+    token_type,
+    expires_in
+  })
+
+  ustream.channel.list()
+    .then((channels) => {
+      const channelData = Object.keys(channels.data).map((id) => {
+        return channels.data[id]
+      })
+
+      res.render('channels', {
+        success: true,
+        channels: channelData
+      })
+    })
+    .catch(function () {
+      res.render('channels', {
+        success: false
+      })
+    })
+})
+
+app.listen(3000)
